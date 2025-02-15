fa4c9405fa927454c7fd9963bbe57eff41e46b01
KRblL <kZhokhov04@mail.ru>
2025-02-15 22:41:29 +0300

    Refactor

--- main.py
+++ main.py
@@ -10,31 +10,39 @@ USERNAME = os.getenv('ROCKET_CHAT_USER')
 PASSWORD = os.getenv('ROCKET_CHAT_PASSWORD')
 SERVER_URL = os.getenv('ROCKET_CHAT_URL')
 
-NEW_USER_EMAIL="user1@mail.ru"
-NEW_USER_NAME="NEW_USER1"
-NEW_USER_PASSWORD="password1"
-NEW_USER_USERNAME="testuser1"
+NEW_EMAIL="user@mail.ru"
+NEW_NAME="NEW_USER"
+NEW_PASSWORD="password"
+NEW_USERNAME="testuser"
+
+
+def print_start_info(rocket: RocketChat) -> None:
+    pprint(rocket.me().json())
+    pprint(rocket.channels_list().json())
+    pprint(rocket.chat_post_message('Hello, World!', channel='GENERAL').json())
+    pprint(rocket.channels_history('GENERAL', count=10).json())
+
+
+def create_user(rocket: RocketChat, email: str, name: str, password: str, username: str) -> str:
+    create_user_response = rocket.users_create(
+        email=email,
+        name=name,
+        password=password,
+        username=username,
+        active=True, 
+        roles=['user'] 
+    )
+    return create_user_response.json().get('user', {}).get('_id')
+
+
+def print_user_info(rocket: RocketChat, id: str) -> None:
+    pprint(rocket.users_info(user_id=id).json())
 
 
 if __name__ == "__main__":
     with sessions.Session() as session:
         rocket = RocketChat(USERNAME, PASSWORD, SERVER_URL, session=session)
-        pprint(rocket.me().json())
-        pprint(rocket.channels_list().json())
-        pprint(rocket.chat_post_message('Hello, World!', channel='GENERAL').json())
-        pprint(rocket.channels_history('GENERAL', count=5).json())
-
-        create_user_response = rocket.users_create(
-            email=NEW_USER_EMAIL,
-            name=NEW_USER_NAME,
-            password=NEW_USER_PASSWORD,
-            username=NEW_USER_USERNAME,
-            active=True, 
-            roles=['user'] 
-        )
-        pprint(create_user_response.json())
-
-        new_user_id = create_user_response.json().get('user', {}).get('_id')    
-        print(f"New user created with ID: {new_user_id}")
-        user_info_response = rocket.users_info(user_id=new_user_id)
-        pprint(user_info_response.json())
+        print_start_info(rocket)
+        user_id = create_user(rocket, NEW_EMAIL, NEW_NAME, NEW_PASSWORD, NEW_USERNAME)
+        print_user_info(rocket, user_id)
+    
