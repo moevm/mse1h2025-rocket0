b8e2b2cfa93f76f43c11cc4e6dd328d4050351b2
yaQvadrat <yaqvadrat@mail.ru>
2025-02-18 00:38:14 +0300

    grooming

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ load_dotenv()
 USERNAME = os.getenv('ROCKET_CHAT_USER')
 PASSWORD = os.getenv('ROCKET_CHAT_PASSWORD')
 SERVER_URL = os.getenv('ROCKET_CHAT_URL')
+MONGO_URL = os.getenv('MONGO_URL_FOR_APP')
 
 NEW_EMAIL="user@mail.ru"
 NEW_NAME="NEW_USER"
@@ -46,12 +47,11 @@ if __name__ == "__main__":
         print_start_info(rocket)
         user_id = create_user(rocket, NEW_EMAIL, NEW_NAME, NEW_PASSWORD, NEW_USERNAME)
         print_user_info(rocket, user_id)
-        mongo_url = os.getenv('MONGO_URL_FOR_APP')
-        client = MongoClient(mongo_url)
+
+        client = MongoClient(MONGO_URL)
         db = client['mydatabase']
         collection = db['mycollection']
         document = {"name": "ars", "age": 20}
         result = collection.insert_one(document)
         print(f"Документ вставлен с ID: {result.inserted_id}")
         client.close()
-        
