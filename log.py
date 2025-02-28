1ef27fe1558d3910b063d036b05f4f09958ce69a
Leonid Ivashinnikov <lus550@yandex.ru>
2025-02-28 16:06:14 +0300

    improved log snippet

--- log.py
+++ log.py
@@ -1,15 +1,22 @@
 import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 def foo(m: int, n: int) -> int:
-    logging.debug('foo(%d, %d)', m, n)
+    logger.debug('foo(%d, %d)', m, n)
 
     if m < 0 or n < 0:
-        logging.error('values are less than zero (%d, %d)', m, n)
+        logger.error('values are less than zero (%d, %d)', m, n)
 
     return m * n
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    handler = logging.FileHandler(f"{__name__}.log", mode='w')
+    formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     foo(5, 5)
     foo(-4, 5)
