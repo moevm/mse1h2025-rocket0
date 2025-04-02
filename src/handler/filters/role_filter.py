c6f6f75ff0a348f22617908a415f8bbf18ca9fcc
KRblL <kZhokhov04@mail.ru>
2025-04-02 16:27:09 +0300

    fix RoleFilter

--- src/handler/filters/role_filter.py
+++ src/handler/filters/role_filter.py
@@ -1,17 +1,13 @@
 from handler.filters.interface import HandlerFilter
 from models.dto import RequestContext
 from dispatcher import Bot
-from pprint import pprint
+from http import HTTPStatus
 
 
 class RoleFilter(HandlerFilter):
-    def __init__(self, roles: tuple[str]) -> None:
-        self._roles = roles
+    def __init__(self, priviliged_roles: frozenset[str]) -> None:
+        self._priviliged_roles = priviliged_roles
 
     def check(self, ctx: RequestContext, bot: Bot) -> bool:
-        response = bot.sync_client.users_info(user_id=ctx.sender_id)
-        if response.status_code != 200:
-             return False
-        
-        user_roles = response.json().get("user", {}).get("roles", [])
-        return bool(set(self._roles) & set(user_roles))
+        user_roles = bot.get_roles(user_id=ctx.sender_id)
+        return bool(self._priviliged_roles & set(user_roles))
