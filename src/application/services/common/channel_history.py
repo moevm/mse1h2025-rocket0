12529306bedf94e6d2b539554b9908b9686b4683
KRblL <kZhokhov04@mail.ru>
2025-05-23 16:45:59 +0300

    fix get_channel_history

--- src/application/services/common/channel_history.py
+++ src/application/services/common/channel_history.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 from typing import Any
 from dispatcher import Bot
+from models.domain import Channel
 from models.enums import RoomType
 
 
 def get_channel_history(
     bot: Bot,
-    channel: dict[str, str],
+    channel: Channel,
     oldest: datetime = None,
     latest: datetime = None,
 ) -> dict[str, Any]:
-    match channel:
-        case {"_id": id, "t": RoomType.CHANNEL}:
-            return bot.get_channel_history(id, oldest, latest)
-        case {"_id": id, "t": RoomType.GROUP}:
-            return bot.get_group_history(id, oldest, latest)
+     match channel.type:
+        case RoomType.CHANNEL:
+            return bot.get_channel_history(channel.id, oldest, latest)
+        case RoomType.GROUP:
+            return bot.get_group_history(channel.id, oldest, latest)
         case _:
             return {}
